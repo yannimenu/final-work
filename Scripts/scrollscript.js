d67df6f8d38cbe4273a7cb09afbcb5948090381b
yannimenu <Yanni.menu@student.ehb.be>
2022-06-22 18:24:04 +0200

    final touches

--- Scripts/scrollscript.js
+++ Scripts/scrollscript.js
@@ -11,7 +11,7 @@ function scrollFunc(){
    /*Apply classes for slide in bar*/
    scrollpos = window.scrollY;
 
-   if (scrollpos > 1) {
+   if (scrollpos > 250) {
        $("#toggleScroll").hide();
    }
    else {
